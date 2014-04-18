782e4009013bf95d401066919fbb71ce0e8b6c17
Aman Grewal <aman1221@aol.com>
2014-04-17 22:18:05 -0400

    It works with 4 LCDS

--- testing.ino
+++ testing.ino
@@ -1,28 +1,25 @@
-#define myledPin1 8
-#define myledPin2 11
-#define myledPin3 12
-#define yourledPin1 5
-#define yourledPin2 9
-#define yourledPin3 10
-#define herledPin1 4
-#define herledPin2 3
-#define herledPin3 2
-#define Button1 13
-#define Button2 7
-#define Button3 6
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const int numButs = 3;
-const int numLights = 3;
-int yourledPins[] = {yourledPin1, yourledPin2, yourledPin3};
-int myledPins[] = {myledPin1, myledPin2, myledPin3};
-int herledPins[] = {herledPin1, herledPin2, herledPin3};
-int Buttons[] = {Button1, Button2, Button3};
-int yourledStates[] = {HIGH, HIGH, HIGH};
-int myledStates[] = {HIGH, HIGH, HIGH};
-int herledStates[] = {HIGH, HIGH, HIGH};
+#define LCDS 4
+#define RedButton 2
+#define WhiteButton 3
+#define GreenButton 4
+#define BlueButton 5
 
-long prev[] = {0, 0, 0};
-long interval[] = {1000, 1000, 1000};
+LiquidCrystal_I2C mylcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+LiquidCrystal_I2C yourlcd(0x26, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C hislcd(0x25, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C herlcd(0x24, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+int lcdVals[] = {0, 0, 0, 0};
+LiquidCrystal_I2C lcds[] = {mylcd, yourlcd, hislcd, herlcd};
+
+const int NumButs = 4;
+int Buts[] = {RedButton, WhiteButton, GreenButton, BlueButton};
+
+long prev[] = {0, 0, 0, 0};
+long interval[] = {1000, 1000, 1000, 1000};
 long minint = 1000;
 long maxint = 5000;
 
@@ -30,69 +27,87 @@ boolean Play = true;
 
 void checkButtons(unsigned long cur)
 {
-  for(int i=0; i<numButs; ++i){
+  for(int i=0; i<LCDS; ++i){
     if(cur-prev[i]>interval[i]){
       interval[i] = random(minint, maxint);
-      minint *= .95;
-      maxint *= .95;
-      if(!(myledStates[i])&&!(yourledStates[i])&&!(herledStates[i])){
-        int x = random(0,3);
-        if(x==0){
-          myledStates[i] = HIGH;
-        }else if(x==1){
-          yourledStates[i]=HIGH;
-        } else if(x==2){
-          herledStates[i]=HIGH;
-        }
+      minint *= .99;
+      maxint *= .99;
+      if(!(lcdVals[i])){
+        lcdVals[i] = random(2,6);
       }else{
         Play = false;
+        hislcd.clear();
+        hislcd.setCursor(0,0);
+        hislcd.write(lcdVals[0]);
       }
       prev[i] = cur;
     }
-    if(digitalRead(Buttons[i])){
+  }
+  for(int i = 0; i<NumButs; ++i){
+    if(digitalRead(Buts[i])){
       delay(1); //for stability
-      myledStates[i] = LOW;
-      yourledStates[i] = LOW;
-      herledStates[i] = LOW;
+      for(int j =0; j<LCDS; ++j){
+        if(lcdVals[j] == (i+2)) { 
+          lcds[j].clear();
+          lcdVals[j] = 0;
+        }
+      }
     }
   }
 }
 
-void writeLights()
+void writeLCD()
 {
-  for(int i=0; i<numLights; ++i){
-    digitalWrite(myledPins[i], myledStates[i]);
-    digitalWrite(yourledPins[i], yourledStates[i]);
-    digitalWrite(herledPins[i], herledStates[i]);
+  for(int i=0; i<LCDS; ++i){
+    lcds[i].clear();
+    switch(lcdVals[i]){
+      case 2:
+        lcds[i].setCursor(0, 0);
+        lcds[i].write("Push the Red");
+        lcds[i].setCursor(5, 1);
+        lcds[i].write("Button");
+        break;
+      case 3:
+        lcds[i].setCursor(0, 0);
+        lcds[i].write("Push the White");
+        lcds[i].setCursor(5, 1);
+        lcds[i].write("Button");
+        break;
+      case 4:
+        lcds[i].setCursor(0, 0);
+        lcds[i].write("Push the Green");
+        lcds[i].setCursor(5, 1);
+        lcds[i].write("Button");
+        break;
+      case 5:
+        lcds[i].setCursor(0, 0);
+        lcds[i].write("Push the Blue");
+        lcds[i].setCursor(5, 1);
+        lcds[i].write("Button");
+        break;
+    }
   }
 }
 
 void lose()
 {
-    for(int i = 0; i<numButs; ++i){
-      myledStates[i] = HIGH;
-      yourledStates[i] = HIGH;
-      herledStates[i] = HIGH;
-    }
-    while(true){
-      writeLights();
-      for(int i = 0; i<numButs; ++i){
-        myledStates[i] = !(myledStates[i]);
-        yourledStates[i] = !(yourledStates[i]);
-        herledStates[i] = !(herledStates[i]);      }
-      delay(300);
-    }
+  for(int i = 0; i<LCDS; ++i){
+    lcds[i].clear();
+    lcds[i].setCursor(0,0);
+    lcds[i].write("You Lose!");
+  }
+  delay(1000);
 }
   
 void setup()
 {
-  for(int i = 0; i<numButs; ++i){
-    pinMode(myledPins[i], OUTPUT);
-    pinMode(yourledPins[i], OUTPUT);
-    pinMode(herledPins[i], OUTPUT);
-    pinMode(Buttons[i], INPUT);
-  } 
-  writeLights();
+  for(int i = 0; i<NumButs; ++i){
+    pinMode(Buts[i], INPUT);
+  }
+  for(int i = 0; i<LCDS; ++i){
+    lcds[i].begin(16, 2);
+  }
+  
 }
 
 void loop()
@@ -101,7 +116,8 @@ void loop()
    unsigned long cur = millis();
    checkButtons(cur);
    delay(1); //for stability when reading buttons
-   writeLights();
+   writeLCD();
+   delay(15);
   } else {
     lose();
   }
