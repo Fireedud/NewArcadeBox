7551380a25bc4bce5062f515fe2b05f012c94429
Aman Grewal <aman1221@aol.com>
2014-04-18 13:18:41 -0400

    Revert "Revert "Fixed screen issues""
    This reverts commit 90530a0c68ed9c9097caeb0c0b89f3d1c69f39ab.

--- testing.ino
+++ testing.ino
@@ -13,6 +13,7 @@ LiquidCrystal_I2C hislcd(0x25, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 LiquidCrystal_I2C herlcd(0x24, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int lcdVals[] = {0, 0, 0, 0};
+boolean has_changed[] = {false, false, false, false};
 LiquidCrystal_I2C lcds[] = {mylcd, yourlcd, hislcd, herlcd};
 
 const int NumButs = 4;
@@ -30,17 +31,17 @@ void checkButtons(unsigned long cur)
   for(int i=0; i<LCDS; ++i){
     if(cur-prev[i]>interval[i]){
       interval[i] = random(minint, maxint);
-      minint *= .99;
-      maxint *= .99;
+      minint *= .98;
+      maxint *= .98;
       if(!(lcdVals[i])){
         lcdVals[i] = random(2,6);
+        has_changed[i]=true;
       }else{
         Play = false;
-        hislcd.clear();
-        hislcd.setCursor(0,0);
-        hislcd.write(lcdVals[0]);
       }
       prev[i] = cur;
+    } else {
+      has_changed[i]=false;
     }
   }
   for(int i = 0; i<NumButs; ++i){
@@ -59,32 +60,34 @@ void checkButtons(unsigned long cur)
 void writeLCD()
 {
   for(int i=0; i<LCDS; ++i){
-    lcds[i].clear();
-    switch(lcdVals[i]){
-      case 2:
-        lcds[i].setCursor(0, 0);
-        lcds[i].write("Push the Red");
-        lcds[i].setCursor(5, 1);
-        lcds[i].write("Button");
-        break;
-      case 3:
-        lcds[i].setCursor(0, 0);
-        lcds[i].write("Push the White");
-        lcds[i].setCursor(5, 1);
-        lcds[i].write("Button");
-        break;
-      case 4:
-        lcds[i].setCursor(0, 0);
-        lcds[i].write("Push the Green");
-        lcds[i].setCursor(5, 1);
-        lcds[i].write("Button");
-        break;
-      case 5:
-        lcds[i].setCursor(0, 0);
-        lcds[i].write("Push the Blue");
-        lcds[i].setCursor(5, 1);
-        lcds[i].write("Button");
-        break;
+    if(has_changed[i]){
+      lcds[i].clear();
+      switch(lcdVals[i]){
+        case 2:
+          lcds[i].setCursor(2, 0);
+          lcds[i].write("Push the Red");
+          lcds[i].setCursor(5, 1);
+          lcds[i].write("Button");
+          break;
+        case 3:
+          lcds[i].setCursor(1, 0);
+          lcds[i].write("Push the White");
+          lcds[i].setCursor(5, 1);
+          lcds[i].write("Button");
+          break;
+        case 4:
+          lcds[i].setCursor(1, 0);
+          lcds[i].write("Push the Green");
+          lcds[i].setCursor(5, 1);
+          lcds[i].write("Button");
+          break;
+        case 5:
+          lcds[i].setCursor(1, 0);
+          lcds[i].write("Push the Blue");
+          lcds[i].setCursor(5, 1);
+          lcds[i].write("Button");
+          break;
+      }
     }
   }
 }
@@ -96,7 +99,7 @@ void lose()
     lcds[i].setCursor(0,0);
     lcds[i].write("You Lose!");
   }
-  delay(1000);
+  delay(10000);
 }
   
 void setup()
