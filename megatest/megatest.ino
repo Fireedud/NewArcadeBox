95c467862d21bf5f814edcbc85c9663f999ba16e
Aman Grewal <aman1221@aol.com>
2014-05-05 18:34:08 -0400

    I need to have a remote version of this

--- megatest/megatest.ino
+++ megatest/megatest.ino
@@ -18,7 +18,7 @@
 // Set the pins on the I2C chip used for LCD connections:
 //                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
 LiquidCrystal_I2C mylcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
-//LiquidCrystal_I2C yourlcd(0x26, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C yourlcd(0x26, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 //LiquidCrystal_I2C hislcd(0x25, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 //LiquidCrystal_I2C herlcd(0x24, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
@@ -26,18 +26,36 @@ LiquidCrystal_I2C mylcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD
 /*-----( Declare Variables )-----*/
 //none
 
-struct switches{
+/*struct things{
   int pinNumber;
+  const char type;
   int cur;
   int desired;
+  const char message1[17];
+  const char message2[17];
 };
 
-struct switches myswitch = {6, LOW, HIGH};
+const int numinputs = 7;
+struct things FILLER = {0, '\0', LOW, LOW, "", ""};
+struct things WhiteButton = {2, 'b', LOW, HIGH, "Push White", ""};
+struct things Covered1 = {3, 's', LOW, HIGH, "Flip Covered1", ""};
+struct things Covered2 = {4, 's', LOW, HIGH, "Flip Covered2", ""};
+struct things RedSwitch = {5, 's', LOW, HIGH, "Flip Red", ""};
+struct things BlueSwitch = {6, 's', LOW, HIGH, "Flip Blue", ""};
+struct things BlueButton  = {7, 'b', LOW, HIGH, "Push Blue", ""};
+struct things RedButton = {8, 'b', LOW, HIGH, "Push Red", ""};
+
+const int length = 9; //length of the array 
+struct things everything[] = {FILLER, FILLER, WhiteButton, Covered1, 
+  Covered2, RedSwitch, BlueSwitch, BlueButton, RedButton}; */
+
 
 void setup()   /*----( SETUP: RUNS ONCE )----*/
 {
-  mylcd.begin(16,2);         // initialize the lcd for 16 chars 2 lines and turn on backlight
-  pinMode(6, INPUT);
+  mylcd.begin(16,2);
+  yourlcd.begin(16,2);
+  pinMode(A0, INPUT);
+  //pinMode(A0, INPUT);
   //yourlcd.begin(16,2);
   //hislcd.begin(16,2);
   //herlcd.begin(16,2);
@@ -46,21 +64,60 @@ void setup()   /*----( SETUP: RUNS ONCE )----*/
 
 void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
 {
-  mylcd.clear();
-  myswitch.cur = digitalRead(myswitch.pinNumber);
   mylcd.setCursor(0,0);
-  if(myswitch.cur){
-    mylcd.write("ON");
-  } else{
-    mylcd.write("OFF!");
-  }
+  mylcd.write("here1");
+  delay(10);
+  yourlcd.setCursor(0,0);
+  yourlcd.write("here1");
+  delay(10);
+  int val = analogRead(0);
+  delay(10);
   mylcd.setCursor(0,1);
-  if(myswitch.cur == myswitch.desired){
-    mylcd.write("MATCHES");
-    myswitch.desired = !myswitch.desired;
-  }else{
-    mylcd.write("DOESN'T");
-  }
-  delay(500);
-}
+  char t[6];
+  itoa(val, t, 10);
+  mylcd.write(t);
+  delay(10);
+  yourlcd.setCursor(0,1);
+  yourlcd.write("Here2");  
+  delay(200);
+  mylcd.clear();
+  yourlcd.clear();
 
+  
+  //N.B.:
+  //North is red
+  //West is black
+  //East is green
+  //South is yellow
+  /*if(a&&b){
+    Serial.println("SW");
+  }else if(a&&d){
+    Serial.println("SE");
+  }else if(a){
+    Serial.println("S");
+  }else if(c&&b){
+    Serial.println("NW");
+  }else if(c&&d){
+    Serial.println("NE");
+  }else if(c){
+    Serial.println("N");
+  }else if(b){
+    Serial.println("W");
+  }else if(d){
+    Serial.println("E");
+  } */
+  //int val = analogRead(A0);
+  //Serial.print("value: ");
+  //Serial.print(val);
+  //Serial.print("\n");
+  //if(val<20){    
+    //Serial.println(0);
+  //} else if(815<val && val<920){
+    //Serial.println(1);
+  //} else if(1000<val){
+    //Serial.println(2);
+  //} else {
+    //Serial.println(3);
+  //}
+  delay(300);
+}
--- testing.ino
+++ testing.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
 
 #define LCDS 2
 
@@ -13,31 +14,33 @@ struct things{
 };
 
 struct things FILLER = {0, '\0', LOW, LOW, "", ""};
-/*struct things WhiteButton = {2, 'b', LOW, HIGH, "Push White", ""};
-struct things Covered1 = {3, 's', LOW, HIGH, "Flip Covered1", ""};
-struct things Covered2 = {4, 's', LOW, HIGH, "Flip Covered2", ""};
-struct things RedSwitch = {5, 's', LOW, HIGH, "Flip Red", ""};
-struct things BlueSwitch = {6, 's', LOW, HIGH, "Flip Blue", ""};
-struct things BlueButton  = {7, 'b', LOW, HIGH, "Push Blue", ""};
-struct things RedButton = {8, 'b', LOW, HIGH, "Push Red", ""};*/
+//struct things WhiteButton = {2, 'b', LOW, HIGH, "Push White", ""};
+struct things Covered1 = {2, 's', LOW, HIGH, "LIGHTS!!", ""};
+struct things Covered2 = {3, 's', LOW, HIGH, "CAMERA!!!", ""};
+//struct things RedSwitch = {5, 's', LOW, HIGH, "Flip Red", ""};
+//struct things BlueSwitch = {6, 's', LOW, HIGH, "Flip Blue", ""};
+//struct things BlueButton  = {7, 'b', LOW, HIGH, "Push Blue", ""};
+//struct things RedButton = {8, 'b', LOW, HIGH, "Push Red", ""};
 struct things Pot = {11, 'p', 0, 2, "Slide to", ""}; //NOTE: not its actual pin
-struct things South = {2, 'b', LOW, HIGH, "South", ""};
-struct things West = {3, 'b', LOW, HIGH, "West", ""};
-struct things North = {4, 'b', LOW, HIGH, "North", ""};
-struct things East = {5, 'b', LOW, HIGH, "East", ""};
+struct things South = {4, 'b', LOW, HIGH, "South", ""};
+struct things West = {6, 'b', LOW, HIGH, "West", ""};
+struct things North = {5, 'b', LOW, HIGH, "North", ""};
+struct things East = {7, 'b', LOW, HIGH, "East", ""};
 //The pin is just a filler. It's the same as its index in everything[]
-struct things NE = {6, '2', LOW, 0, "Northeast", ""};
+struct things NE = {10, '2', LOW, 0, "Northeast", ""};
 struct things NW = {7, '2', LOW, 1, "Northwest", ""};
 struct things SouthEast = {8, '2', LOW, 2, "Southeast", ""};
 struct things SW = {9, '2', LOW, 3, "Southwest", ""};
-struct things WhiteButton = {10, 'b', LOW, HIGH, "White", ""};
+//struct things WhiteButton = {10, 'b', LOW, HIGH, "White", ""};
 
 //make sure array index matches pinNumber
-const int length = 12;
-struct things everything[] = {FILLER, FILLER, South, West, 
-  North, East, NE, NW, SouthEast, SW, WhiteButton, Pot};
-//struct things everything[] = {FILLER, FILLER, WhiteButton, Covered1, 
-  //Covered2, RedSwitch, BlueSwitch, BlueButton, RedButton, Pot}; 
+const int length = 10;
+struct things everything[] = {FILLER, FILLER, Covered1, Covered2, South, North, West, East, SouthEast, SW, NE};
+
+Adafruit_NeoPixel mystrip = Adafruit_NeoPixel(15, 8, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel yourstrip = Adafruit_NeoPixel(15, 9, NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel strips[] = {mystrip, yourstrip};
 
 LiquidCrystal_I2C mylcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
 LiquidCrystal_I2C yourlcd(0x26, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
@@ -45,7 +48,7 @@ LiquidCrystal_I2C hislcd(0x25, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 LiquidCrystal_I2C herlcd(0x24, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int lcdVals[] = {0, 0, 0, 0};
-LiquidCrystal_I2C lcds[] = {mylcd, yourlcd, hislcd, herlcd};
+LiquidCrystal_I2C lcds[] = {yourlcd, herlcd};
 
 long prev[] = {0, 0, 0, 0};
 long interval[] = {2000, 2000, 2000, 2000};
@@ -53,6 +56,295 @@ long minint = 2000;
 long maxint = 5000;
 
 boolean Play = true;
+
+void colors(long hunter, int i)  
+{
+  if(hunter > 0){ 
+      strips[i].setPixelColor(0, 0, 255, 0);
+      strips[i].setPixelColor(1, 0, 255, 0);
+      strips[i].setPixelColor(2, 0, 255, 0);
+      strips[i].setPixelColor(3,  0, 255, 0);
+      strips[i].setPixelColor(4,  0, 255, 0);
+      strips[i].setPixelColor(5, 0, 255, 0);
+      strips[i].setPixelColor(6,  0, 255, 0);
+      strips[i].setPixelColor(7,  0, 255, 0);
+      strips[i].setPixelColor(8,  0, 255, 0);
+      strips[i].setPixelColor(9,  0, 255, 0);
+      strips[i].setPixelColor(10,  0, 255, 0);
+      strips[i].setPixelColor(11,  0, 255, 0);
+      strips[i].setPixelColor(12,  0, 255, 0);
+      strips[i].setPixelColor(13,  0, 255, 0);
+      strips[i].setPixelColor(14,  0, 255, 0);
+  }
+  
+  if(hunter > 1000){
+      strips[i].setPixelColor(0, 17, 238, 0);
+      strips[i].setPixelColor(1, 17, 238, 0);
+      strips[i].setPixelColor(2, 17, 238, 0);
+      strips[i].setPixelColor(3, 17, 238, 0);
+      strips[i].setPixelColor(4, 17, 238, 0);
+      strips[i].setPixelColor(5, 17, 238, 0);
+      strips[i].setPixelColor(6, 17, 238, 0);
+      strips[i].setPixelColor(7, 17, 238, 0);
+      strips[i].setPixelColor(8, 17, 238, 0);
+      strips[i].setPixelColor(9, 17, 238, 0);
+      strips[i].setPixelColor(10, 17, 238, 0);
+      strips[i].setPixelColor(11, 17, 238, 0);
+      strips[i].setPixelColor(12, 17, 238, 0);
+      strips[i].setPixelColor(13, 17, 238, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  
+  if(hunter > 2000){
+      strips[i].setPixelColor(0, 34, 221, 0);
+      strips[i].setPixelColor(1, 34, 221, 0);
+      strips[i].setPixelColor(2, 34, 221, 0);
+      strips[i].setPixelColor(3, 34, 221, 0);
+      strips[i].setPixelColor(4, 34, 221, 0);
+      strips[i].setPixelColor(5, 34, 221, 0);
+      strips[i].setPixelColor(6, 34, 221, 0);
+      strips[i].setPixelColor(7, 34, 221, 0);
+      strips[i].setPixelColor(8, 34, 221, 0);
+      strips[i].setPixelColor(9, 34, 221, 0);
+      strips[i].setPixelColor(10, 34, 221, 0);
+      strips[i].setPixelColor(11, 34, 221, 0);
+      strips[i].setPixelColor(12, 34, 221, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  if(hunter > 3000){
+      strips[i].setPixelColor(0, 51, 204, 0);
+      strips[i].setPixelColor(1, 51, 204, 0); 
+      strips[i].setPixelColor(2, 51, 204, 0);
+      strips[i].setPixelColor(3, 51, 204, 0);
+      strips[i].setPixelColor(4, 51, 204, 0);
+      strips[i].setPixelColor(5, 51, 204, 0);
+      strips[i].setPixelColor(6, 51, 204, 0);
+      strips[i].setPixelColor(7, 51, 204, 0);
+      strips[i].setPixelColor(8, 51, 204, 0);
+      strips[i].setPixelColor(9, 51, 204, 0);
+      strips[i].setPixelColor(10, 51, 204, 0);
+      strips[i].setPixelColor(11, 51, 204, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  if(hunter > 4000){
+      strips[i].setPixelColor(0, 68, 187, 0);
+      strips[i].setPixelColor(1, 68, 187, 0);
+      strips[i].setPixelColor(2, 68, 187, 0);
+      strips[i].setPixelColor(3, 68, 187, 0);
+      strips[i].setPixelColor(4, 68, 187, 0);
+      strips[i].setPixelColor(5, 68, 187, 0);
+      strips[i].setPixelColor(6, 68, 187, 0);
+      strips[i].setPixelColor(7, 68, 187, 0);
+      strips[i].setPixelColor(8, 68, 187, 0);
+      strips[i].setPixelColor(9, 68, 187, 0);
+      strips[i].setPixelColor(10, 68, 187, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  if (hunter > 5000)
+  {
+      strips[i].setPixelColor(0, 85, 170, 0);
+      strips[i].setPixelColor(1, 85, 170, 0);
+      strips[i].setPixelColor(2, 85, 170, 0);
+      strips[i].setPixelColor(3, 85, 170, 0);
+      strips[i].setPixelColor(4, 85, 170, 0);
+      strips[i].setPixelColor(5, 85, 170, 0);
+      strips[i].setPixelColor(6, 85, 170, 0);
+      strips[i].setPixelColor(7, 85, 170, 0);
+      strips[i].setPixelColor(8, 85, 170, 0);
+      strips[i].setPixelColor(9, 85, 170, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  if (hunter > 6000)
+  {
+      strips[i].setPixelColor(0, 102, 153, 0);
+      strips[i].setPixelColor(1, 102, 153, 0);
+      strips[i].setPixelColor(2, 102, 153, 0);
+      strips[i].setPixelColor(3, 102, 153, 0);
+      strips[i].setPixelColor(4, 102, 153, 0);
+      strips[i].setPixelColor(5, 102, 153, 0);
+      strips[i].setPixelColor(6, 102, 153, 0);
+      strips[i].setPixelColor(7, 102, 153, 0);
+      strips[i].setPixelColor(8, 102, 153, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  if (hunter > 7000)
+  {
+      strips[i].setPixelColor(0, 119, 136, 0);
+      strips[i].setPixelColor(1, 119, 136, 0);
+      strips[i].setPixelColor(2, 119, 136, 0);
+      strips[i].setPixelColor(3, 119, 136, 0);
+      strips[i].setPixelColor(4, 119, 136, 0);
+      strips[i].setPixelColor(5, 119, 136, 0);
+      strips[i].setPixelColor(6, 119, 136, 0);
+      strips[i].setPixelColor(7, 119, 136, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+ if (hunter > 8000)
+  {
+      strips[i].setPixelColor(0, 136, 119, 0);
+      strips[i].setPixelColor(1, 136, 119, 0);
+      strips[i].setPixelColor(2, 136, 119, 0);
+      strips[i].setPixelColor(3, 136, 119, 0);
+      strips[i].setPixelColor(4, 136, 119, 0);
+      strips[i].setPixelColor(5, 136, 119, 0);
+      strips[i].setPixelColor(6, 136, 119, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+}
+ if (hunter > 9000)
+  {
+      strips[i].setPixelColor(0, 153, 102, 0);
+      strips[i].setPixelColor(1, 153, 102, 0);
+      strips[i].setPixelColor(2, 153, 102, 0);
+      strips[i].setPixelColor(3, 153, 102, 0);
+      strips[i].setPixelColor(4, 153, 102, 0);
+      strips[i].setPixelColor(5, 153, 102, 0);
+      strips[i].setPixelColor(6, 0, 0, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+ if (hunter > 10000)
+  {
+      strips[i].setPixelColor(0, 170, 85, 0);
+      strips[i].setPixelColor(1, 170, 85, 0);
+      strips[i].setPixelColor(2, 170, 85, 0);
+      strips[i].setPixelColor(3, 170, 85, 0);
+      strips[i].setPixelColor(4, 170, 85, 0);
+      strips[i].setPixelColor(5, 0, 0, 0);
+      strips[i].setPixelColor(6, 0, 0, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+if (hunter > 11000)
+  {
+      strips[i].setPixelColor(0, 187, 68, 0);
+      strips[i].setPixelColor(1, 187, 68, 0);
+      strips[i].setPixelColor(2, 187, 68, 0);
+      strips[i].setPixelColor(3, 187, 68, 0);
+      strips[i].setPixelColor(4, 0, 0, 0);
+      strips[i].setPixelColor(5, 0, 0, 0);
+      strips[i].setPixelColor(6, 0, 0, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+if (hunter > 12000)
+  {
+      strips[i].setPixelColor(0, 204, 51, 0);
+      strips[i].setPixelColor(1, 204, 51, 0);
+      strips[i].setPixelColor(2, 204, 51, 0);
+      strips[i].setPixelColor(3, 0, 0, 0);
+      strips[i].setPixelColor(4, 0, 0, 0);
+      strips[i].setPixelColor(5, 0, 0, 0);
+      strips[i].setPixelColor(6, 0, 0, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+ if (hunter > 13000)
+  {
+      strips[i].setPixelColor(0, 221, 34, 0);
+      strips[i].setPixelColor(1, 221, 34, 0);
+      strips[i].setPixelColor(2, 0, 0, 0);
+      strips[i].setPixelColor(3, 0, 0, 0);
+      strips[i].setPixelColor(4, 0, 0, 0);
+      strips[i].setPixelColor(5, 0, 0, 0);
+      strips[i].setPixelColor(6, 0, 0, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+ if (hunter > 14000)
+  {
+      strips[i].setPixelColor(0, 255, 0, 0);
+      strips[i].setPixelColor(1, 0, 0, 0);
+      strips[i].setPixelColor(2, 0, 0, 0);
+      strips[i].setPixelColor(3, 0, 0, 0);
+      strips[i].setPixelColor(4, 0, 0, 0);
+      strips[i].setPixelColor(5, 0, 0, 0);
+      strips[i].setPixelColor(6, 0, 0, 0);
+      strips[i].setPixelColor(7, 0, 0, 0);
+      strips[i].setPixelColor(8, 0, 0, 0);      
+      strips[i].setPixelColor(9, 0, 0, 0);
+      strips[i].setPixelColor(10, 0, 0, 0);
+      strips[i].setPixelColor(11, 0, 0, 0);
+      strips[i].setPixelColor(12, 0, 0, 0);
+      strips[i].setPixelColor(13, 0, 0, 0);
+      strips[i].setPixelColor(14, 0, 0, 0);
+  }
+  if(lcdVals[i] == 0){
+      strips[i].setPixelColor(0, 0, 255, 0);
+      strips[i].setPixelColor(1, 0, 255, 0);
+      strips[i].setPixelColor(2, 0, 255, 0);
+      strips[i].setPixelColor(3,  0, 255, 0);
+      strips[i].setPixelColor(4,  0, 255, 0);
+      strips[i].setPixelColor(5, 0, 255, 0);
+      strips[i].setPixelColor(6,  0, 255, 0);
+      strips[i].setPixelColor(7,  0, 255, 0);
+      strips[i].setPixelColor(8,  0, 255, 0);
+      strips[i].setPixelColor(9,  0, 255, 0);
+      strips[i].setPixelColor(10,  0, 255, 0);
+      strips[i].setPixelColor(11,  0, 255, 0);
+      strips[i].setPixelColor(12,  0, 255, 0);
+      strips[i].setPixelColor(13,  0, 255, 0);
+      strips[i].setPixelColor(14,  0, 255, 0);
+  }
+  strips[i].show();
+}
 
 int newint()
 {
@@ -255,21 +547,76 @@ void lose()
     lcds[i].setCursor(0,0);
     lcds[i].write("You Lose!");
   }
+  while(1){
+    for(int i =0; i< LCDS; ++i){
+      strips[i].setPixelColor(0, 255, 0, 0);
+      strips[i].setPixelColor(1, 255, 0, 0);
+      strips[i].setPixelColor(2, 255, 0, 0);
+      strips[i].setPixelColor(3,  255, 0, 0);
+      strips[i].setPixelColor(4,  255, 0, 0);
+      strips[i].setPixelColor(5, 255, 0, 0);
+      strips[i].setPixelColor(6,  255, 0, 0);
+      strips[i].setPixelColor(7,  255, 0, 0);
+      strips[i].setPixelColor(8,  255, 0, 0);
+      strips[i].setPixelColor(9,  255, 0, 0);
+      strips[i].setPixelColor(10,  255, 0, 0);
+      strips[i].setPixelColor(11,  255, 0, 0);
+      strips[i].setPixelColor(12,  255, 0, 0);
+      strips[i].setPixelColor(13,  255, 0, 0);
+      strips[i].setPixelColor(14,  255, 0, 0);
+      strips[i].show();
+    }
+    delay(500);
+    for(int i =0; i<LCDS; ++i){
+      strips[i].setPixelColor(0, 0, 0, 0);
+      strips[i].setPixelColor(1, 0, 0, 0);
+      strips[i].setPixelColor(2, 0, 0, 0);
+      strips[i].setPixelColor(3,  0, 0, 0);
+      strips[i].setPixelColor(4,  0, 0, 0);
+      strips[i].setPixelColor(5, 0, 0, 0);
+      strips[i].setPixelColor(6,  0, 0, 0);
+      strips[i].setPixelColor(7,  0, 0, 0);
+      strips[i].setPixelColor(8,  0, 0, 0);
+      strips[i].setPixelColor(9,  0, 0, 0);
+      strips[i].setPixelColor(10,  0, 0, 0);
+      strips[i].setPixelColor(11,  0, 0, 0);
+      strips[i].setPixelColor(12,  0, 0, 0);
+      strips[i].setPixelColor(13,  0, 0, 0);
+      strips[i].setPixelColor(14,  0, 0, 0);
+      strips[i].show();
+    }
+    delay(250);
+  }
+      
   Serial.println("lost");
   delay(10000);
 }
   
 void setup()
 {
-  //for(int i = 0; i<length; ++i){
-    //if(everything[i].pinNumber) pinMode(everything[i].pinNumber, INPUT);
-  //}
+  for(int i = 0; i<length; ++i){
+    if(everything[i].pinNumber) pinMode(everything[i].pinNumber, INPUT);
+  }
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
   //pinMode(A0, INPUT); //just double checking
   for(int i = 0; i<LCDS; ++i){
     lcds[i].begin(16, 2);
   }
   Serial.begin(9600);
   Serial.println("Start");
+  for(int i =0; i< LCDS; ++i){
+    strips[i].begin();
+    strips[i].show();
+  }
+}
+
+void health(long cur)
+{
+  for(int i = 0; i < LCDS; ++i){
+    long val = (cur-prev[i])* 15000 / interval[i]; //normalize to hunter's 15 seconds
+    colors(val, i);
+  }
 }
 
 void loop()
@@ -279,6 +626,7 @@ void loop()
    checkButtons(cur);
    delay(1); //for stability when reading buttons
    //writeLCD();
+   health(cur);
    delay(15);
   } else {
     lose();
